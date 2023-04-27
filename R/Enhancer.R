32c32c3e1312cf80f00edcc48625677b2f358902
Xing Qiu <xing.qiu@gmail.com>
2023-04-26 22:44:09 -0400

    1. Implemented subset prediction (via option "predictors") in GEDE. 2. Set version to 0.2.3.

--- R/Enhancer.R
+++ R/Enhancer.R
@@ -1,5 +1,5 @@
 ## This is the proposed enhancing function
-GEDE <- function(Y, Est="auto", HD=FALSE, HD.iter=5, nMAD=3, verbose=FALSE, ...) {
+GEDE <- function(Y, Est="auto", predictors=seq(1:ncol(Y)), HD=FALSE, HD.iter=5, nMAD=3, verbose=FALSE, ...) {
   if (identical(Est,"auto")) Est <- RobEst(Y, HD=HD, HD.iter=HD.iter, ...)
   muhat <- Est$muhat; Tk <- Est$Tk; Lk <- Est$Lk
   sigma2 <- Est$sigma2; K <- Est$K; n <- nrow(Y)
@@ -13,11 +13,24 @@ GEDE <- function(Y, Est="auto", HD=FALSE, HD.iter=5, nMAD=3, verbose=FALSE, ...)
   } else if (K==0) { #no useful, sample-specific signal
     Xhat <- rep(1,n)%*%t(muhat)
   } else { #the main case
-    Ytilde <- sweep(Y.imputed, 2, muhat)%*%Tk
-    Ltilde <- Lk/(sigma2+Lk)
-    Xhat <- rep(1,n)%*%t(muhat) +Ytilde%*%t(sweep(Tk, 2, Ltilde, "*"))
-    dimnames(Xhat) <- dimnames(Y)
+    if (identical(predictors, seq(1:ncol(Y)))) { #auto-prediction
+      ## Ytilde <- sweep(Y.imputed, 2, muhat)%*%Tk
+      ## Ltilde <- Lk/(sigma2+Lk)
+      ## Xhat <- rep(1,n)%*%t(muhat) +Ytilde%*%t(sweep(Tk, 2, Ltilde, "*"))
+      Yc <- sweep(Y.imputed, 2, muhat)
+      Tktilde <- sweep(Tk, 2, Lk/(sigma2+Lk), "*")
+      Xhat <- rep(1,n)%*%t(muhat) +(Yc%*%Tk)%*%t(Tktilde)
+    } else { #using only a subset of predictors
+      Tk2 <- Tk[predictors,]; Y2 <- Y.imputed[,predictors]
+      ss <- svd(sweep(Tk2, 2, sqrt(Lk), "*"))
+      U <- ss$u; V <- ss$v; dd <- ss$d
+      Tktilde <- sweep(Tk, 2, sqrt(Lk), "*")%*%V
+      Utilde <- sweep(U, 2, dd/(sigma2+dd^2), "*")
+      Y2c <- sweep(Y2, 2, muhat[predictors])
+      Xhat <- rep(1,n)%*%t(muhat) +(Y2c%*%Utilde)%*%t(Tktilde)
+    }
   }
+  dimnames(Xhat) <- dimnames(Y)
   if (verbose) {
     ## Add Xhat and out.idx to Est and return Est
     Est$Xhat <- Xhat; Est$out.idx <- out.idx; Est$NA.idx <- which(is.na(Y))
@@ -34,7 +47,7 @@ Enhancer <- function(train, test, method=c("GEDE", "lasso"), predictors=seq(1:nc
   n <- nrow(train); p <- ncol(train)
   if (method=="GEDE"){
     Est <- RobEst(train, ...)
-    Xhat <- GEDE(test, Est=Est, ...)
+    Xhat <- GEDE(test, Est=Est, predictors=predictors, ...)
   } else if (method=="lasso"){
     Xhat <- matrix(0, nrow(test), p)
     for (i in 1:p) {
